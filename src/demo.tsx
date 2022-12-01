6e027b81d4454f6173389f98aae26b34067c9425
Michael Wolf <michael.wolf@copyleft.mx>
2022-12-01 10:12:54 -0600

    Play with demo components

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,8 +1,45 @@
-import * as React from 'react';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
 
-export default function SimpleMediaQuery() {
-  const matches = useMediaQuery('(min-width:600px)');
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+function SimpleMediaQuery() {
+  const matches = useMediaQuery("(min-width:600px)");
 
   return <span>{`(min-width:600px) matches: ${matches}`}</span>;
 }
+
+function MyComponent() {
+  const theme = useTheme();
+
+  const xs = useMediaQuery(theme.breakpoints.up("xs"));
+  const sm = useMediaQuery(theme.breakpoints.up("sm"));
+  const md = useMediaQuery(theme.breakpoints.up("md"));
+  const lg = useMediaQuery(theme.breakpoints.up("lg"));
+  const xl = useMediaQuery(theme.breakpoints.up("xl"));
+
+  return (
+    <div>
+      <div>{`theme.breakpoints.up('xs') matches: ${xs}`}</div>
+      <div>{`theme.breakpoints.up('sm') matches: ${sm}`}</div>
+      <div>{`theme.breakpoints.up('md') matches: ${md}`}</div>
+      <div>{`theme.breakpoints.up('lg') matches: ${lg}`}</div>
+      <div>{`theme.breakpoints.up('xl') matches: ${xl}`}</div>
+    </div>
+  );
+}
+
+const Demo = () => {
+  return (
+    <>
+      <div>
+        <SimpleMediaQuery />
+      </div>
+      <div>
+        <MyComponent />
+      </div>
+    </>
+  );
+};
+
+export { Demo, MyComponent, SimpleMediaQuery };
