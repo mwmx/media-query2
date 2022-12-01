8efd2deec80f55345229b5e2a74a46579e59ed3d
Michael Wolf <michael.wolf@copyleft.mx>
2022-12-01 10:13:10 -0600

    Get thing to actually build

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import Demo from './demo';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { Demo } from "./demo";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <Demo />
     </StyledEngineProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
